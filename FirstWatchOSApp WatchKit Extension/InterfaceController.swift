d66af9df5fe80111f478c64b674ebb8996ef4aa6
juhom205 <80626044+juhom205@users.noreply.github.com>
2022-06-12 16:32:20 +0200

    Add files via upload

---
+++ FirstWatchOSApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  FirstWatchOSApp WatchKit Extension
+//
+//  Created by Julian Hoffmann on 12.06.22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
